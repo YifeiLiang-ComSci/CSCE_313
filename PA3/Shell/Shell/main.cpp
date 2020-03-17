50ffcf288f84a30a6c57d805cecc85ba18c8dd6b
yifeiliang <zx245455600@gmail.com>
2020-03-17 17:04:47 -0500

    Update main.cpp

--- PA3/Shell/Shell/main.cpp
+++ PA3/Shell/Shell/main.cpp
@@ -60,6 +60,7 @@ void execute(string inputline){
 int main(){
     while(true){
         cin.clear();
+        cin.ignore();
         cout << "My Shell$ ";
         string inputline;
         getline(cin,inputline);
