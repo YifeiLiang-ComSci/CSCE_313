75c4e18b4672ca79465b183ab0dacba3713bffa8
yifeiliang <zx245455600@gmail.com>
2020-04-08 13:46:06 -0500

    Update client.cpp

--- PA4/client.cpp
+++ PA4/client.cpp
@@ -204,7 +204,8 @@ int main(int argc, char *argv[])
     }
     gettimeofday (&end, 0);
     // print the results
-	hc.print ();
+    if(!filetransfer)
+	   hc.print ();
     int secs = (end.tv_sec * 1e6 + end.tv_usec - start.tv_sec * 1e6 - start.tv_usec)/(int) 1e6;
     int usecs = (int)(end.tv_sec * 1e6 + end.tv_usec - start.tv_sec * 1e6 - start.tv_usec)%((int) 1e6);
     cout << "Took " << secs << " seconds and " << usecs << " micro seconds" << endl;
